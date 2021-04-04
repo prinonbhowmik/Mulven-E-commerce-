20e3f97dd6221ec4de3fff21bee27d5340626baa
Prinon <prinonarnob@gmail.com>
2021-04-04 18:41:09 +0600

    add to cart

--- app/src/main/java/com/hydertechno/mulven/Models/UserProfile.java
+++ app/src/main/java/com/hydertechno/mulven/Models/UserProfile.java
@@ -6,13 +6,13 @@ public class UserProfile {
     private String phone;
     private String email;
     private String birthday;
-    private int status;
+    private String status;
     private String address;
+    private String updated_at;
     private String created_at;
-    private String access_token;
-    private String message;
+    private String token;
 
-    public UserProfile(int id, String full_name, String phone, String email, String birthday, int status, String address, String created_at, String access_token, String message) {
+    public UserProfile(int id, String full_name, String phone, String email, String birthday, String status, String address, String updated_at, String created_at, String token) {
         this.id = id;
         this.full_name = full_name;
         this.phone = phone;
@@ -20,9 +20,9 @@ public class UserProfile {
         this.birthday = birthday;
         this.status = status;
         this.address = address;
+        this.updated_at = updated_at;
         this.created_at = created_at;
-        this.access_token = access_token;
-        this.message = message;
+        this.token = token;
     }
 
     public UserProfile() {
@@ -48,7 +48,7 @@ public class UserProfile {
         return birthday;
     }
 
-    public int getStatus() {
+    public String getStatus() {
         return status;
     }
 
@@ -56,15 +56,15 @@ public class UserProfile {
         return address;
     }
 
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
     public String getCreated_at() {
         return created_at;
     }
 
-    public String getAccess_token() {
-        return access_token;
-    }
-
-    public String getMessage() {
-        return message;
+    public String getToken() {
+        return token;
     }
 }
