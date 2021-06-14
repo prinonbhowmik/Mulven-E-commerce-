44d61632866ddaa0850a666c7d2fd79fbac1fd90
Riad <dev.riadul@gmail.com>
2021-06-14 11:29:05 +0600

    update

--- app/src/main/java/com/hydertechno/mulven/Fragments/OrderSuccessFragment.java
+++ app/src/main/java/com/hydertechno/mulven/Fragments/OrderSuccessFragment.java
@@ -2,16 +2,22 @@ package com.hydertechno.mulven.Fragments;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.hydertechno.mulven.R;
 
 
 public class OrderSuccessFragment extends DialogFragment {
+    TextView countTextView;
 
     @Override
     public void onStart()
@@ -36,9 +42,25 @@ public class OrderSuccessFragment extends DialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =inflater.inflate(R.layout.fragment_order_success, container, false);
-
-
-
+        countTextView = view.findViewById(R.id.count);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        new CountDownTimer(4000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int seconds = (int) (millisUntilFinished / 1000);
+                countTextView.setText(seconds + "");
+            }
+            @Override
+            public void onFinish() {
+                countTextView.setText("0");
+                OrderSuccessFragment.this.dismiss();
+            }
+        }.start();
     }
 }
