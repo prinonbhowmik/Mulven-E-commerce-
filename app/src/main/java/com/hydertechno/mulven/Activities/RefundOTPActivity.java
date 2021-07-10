53e56cdb47b94193a163451f471be1cd434d42dc
imran656565 <imran656565@gmail.com>
2021-07-10 14:45:25 +0600

    otp verification class update

--- app/src/main/java/com/hydertechno/mulven/Activities/RefundOTPActivity.java
+++ app/src/main/java/com/hydertechno/mulven/Activities/RefundOTPActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.hydertechno.mulven.R;
 
@@ -13,11 +15,21 @@ import java.util.Objects;
 
 public class RefundOTPActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private TextView baEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refund_o_t_p);
         init();
+        baEdit=findViewById(R.id.baEdit);
+        baEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent( RefundOTPActivity.this,RefundSettlementActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     public void init(){
