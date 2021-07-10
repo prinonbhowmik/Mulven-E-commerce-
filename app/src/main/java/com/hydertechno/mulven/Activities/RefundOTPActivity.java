b4add499b15c711e212bba2c4a7c9600cd08eb4f
imran656565 <imran656565@gmail.com>
2021-07-10 14:19:26 +0600

    otp verification class

--- app/src/main/java/com/hydertechno/mulven/Activities/RefundOTPActivity.java
+++ app/src/main/java/com/hydertechno/mulven/Activities/RefundOTPActivity.java
@@ -1,16 +1,54 @@
 package com.hydertechno.mulven.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.hydertechno.mulven.R;
 
-public class RefundOTPActivity extends AppCompatActivity {
+import java.util.Objects;
 
+public class RefundOTPActivity extends AppCompatActivity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refund_o_t_p);
+        init();
     }
+
+    public void init(){
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent=new Intent( RefundOTPActivity.this,MainActivity.class);
+                intent.putExtra("fragment","profile");
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent=new Intent( RefundOTPActivity.this,MainActivity.class);
+        intent.putExtra("fragment","profile");
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+        finish();
+    }
+
 }
